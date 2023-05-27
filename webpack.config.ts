bf71edd72a19eb4935af96eb6046dbca150c6433
salman.khimishev01 <salman960623>
2023-05-27 12:33:49 +0300

    feat: add env environment

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,22 +1,25 @@
 import path from "path";
 import webpack from "webpack";
 import buildWebpackConfig from "./config/build/buildWebpackConfig";
+import { BuildEnv } from "./config/build/types/config";
 
-const mode = "development";
-const isDev = mode === "development";
-const PORT = 3000;
+export default (env: BuildEnv) => {
+  const mode = env.mode || "development";
+  const isDev = mode === "development";
+  const PORT = env.port || 3000;
 
-const paths = {
-  entry: path.resolve(__dirname, "src", "index.ts"),
-  buld: path.resolve(__dirname, "build"),
-  html: path.resolve(__dirname, "public", "index.html"),
+  const paths = {
+    entry: path.resolve(__dirname, "src", "index.ts"),
+    buld: path.resolve(__dirname, "build"),
+    html: path.resolve(__dirname, "public", "index.html"),
+  };
+
+  const config: webpack.Configuration = buildWebpackConfig({
+    mode,
+    paths,
+    isDev,
+    port: PORT,
+  });
+
+  return config;
 };
-
-const config: webpack.Configuration = buildWebpackConfig({
-  mode,
-  paths,
-  isDev,
-  port: PORT,
-});
-
-export default config;
