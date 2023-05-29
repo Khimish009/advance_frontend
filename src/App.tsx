d8b1d6ea69f7154ea2847d95ba03913217d3fce6
salman.khimishev01 <salman960623>
2023-05-29 20:52:06 +0300

    feat: add routing to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import { Routes, Route, Link } from 'react-router-dom'
 import './index.scss'
+import AboutPage from './pages/AboutPage/AboutPage'
+import MainPage from './pages/MainPage/MainPage'
 
 const App = () => {
 
-  return <div>Hello World!!!</div>
+  return (
+    <div>
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О сайте</Link>
+      <Routes>
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/' element={<MainPage />} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
