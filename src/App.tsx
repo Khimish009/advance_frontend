7d127ec139fb4306c66c0a520af8d030fa4c4080
salman.khimishev01 <salman960623>
2023-05-30 21:37:21 +0300

    feat: toggle theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 import { Routes, Route, Link } from 'react-router-dom'
 import './styles/index.scss'
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
 
+export enum Theme {
+  LIGHT = 'light',
+  DARK = 'dark'
+}
+
 const App = () => {
+  const [theme, setTheme] = useState<Theme>(Theme.LIGHT)
+
+  const toggleTheme = () => {
+    setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK)
+  }
 
   return (
-    <div className='app'>
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle theme</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
       <Suspense fallback={<div>Загрузка...</div>}>
