4b8e4f660a62ec4491933a0d95458cf238156cf1
salman.khimishev01 <salman960623>
2023-05-30 21:08:50 +0300

    feat: add reset styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Suspense } from 'react';
 import { Routes, Route, Link } from 'react-router-dom'
-import './index.scss'
+import './styles/index.scss'
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
 
 const App = () => {
 
   return (
-    <div>
+    <div className='app'>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
       <Suspense fallback={<div>Загрузка...</div>}>
