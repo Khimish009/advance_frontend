4ff5705e04facced677dede21f5b657f407ac7ca
salman.khimishev01 <salman960623>
2023-06-01 10:31:47 +0300

    feat: create use theme hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Suspense, useContext } from 'react';
+import { Suspense } from 'react';
 import { Routes, Route, Link } from 'react-router-dom'
 import './styles/index.scss'
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
-import { ThemeContext } from './theme/ThemeContext';
+import { useTheme } from './theme/useTheme';
 
 export enum Theme {
   LIGHT = 'light',
@@ -11,11 +11,7 @@ export enum Theme {
 }
 
 const App = () => {
-  const { theme, setTheme} = useContext(ThemeContext)
-
-  const toggleTheme = () => {
-    setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK)
-  }
+  const { theme, toggleTheme } = useTheme()
 
   return (
     <div className={`app ${theme}`}>
