b033ac914fdbd3d814caa7cf6b3c923d663010c9
salman.khimishev01 <salman960623>
2023-06-01 10:17:30 +0300

    feat: create theme context

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { Suspense, useState } from 'react';
+import { Suspense, useContext } from 'react';
 import { Routes, Route, Link } from 'react-router-dom'
 import './styles/index.scss'
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { ThemeContext } from './theme/ThemeContext';
 
 export enum Theme {
   LIGHT = 'light',
@@ -10,7 +11,7 @@ export enum Theme {
 }
 
 const App = () => {
-  const [theme, setTheme] = useState<Theme>(Theme.LIGHT)
+  const { theme, setTheme} = useContext(ThemeContext)
 
   const toggleTheme = () => {
     setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK)
